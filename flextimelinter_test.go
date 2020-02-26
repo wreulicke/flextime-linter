b8552f7194dd2b67026d046d422a7d94139b80bb
wreulicke <masaya2dies@gmail.com>
2020-02-27 01:09:15 +0900

    fix test

--- flextimelinter_test.go
+++ flextimelinter_test.go
@@ -1,28 +1,12 @@
 package linter
 
 import (
-	"go/ast"
-	"go/parser"
-	"go/token"
 	"testing"
 
 	"golang.org/x/tools/go/analysis/analysistest"
 )
 
-func TestPrint(t *testing.T) {
-	fset := token.NewFileSet()
-	f, err := parser.ParseFile(fset, "testdata/src/a/a.go", nil, parser.Mode(0))
-	if err != nil {
-		t.Fatal(err)
-	}
-	for _, d := range f.Decls {
-		ast.Print(fset, d)
-	}
-	t.Error("test")
-}
-
 func Test(t *testing.T) {
 	testdata := analysistest.TestData()
 	analysistest.Run(t, testdata, Analyzer, "a")
-	t.Error("test")
 }
